80c0eb4ff8f052cf6e158bf52859c1bbcafa2e80
jeremiejig <me@jeremiejig.fr>
2015-09-13 15:57:18 +0200

    if is a block moving local var outside

--- fastupdate.fish
+++ fastupdate.fish
@@ -10,10 +10,11 @@ function init -a path --on-event init_fastupdate
 end
 
 function fastupdate -d "FastUpdate Print the git version"
+	set -xl GIT_WORK_TREE
 	if test -d $OMF_PATH/pkg/fastupdate
-		set -xl GIT_WORK_TREE $OMF_PATH/pkg/fastupdate
+		set GIT_WORK_TREE $OMF_PATH/pkg/fastupdate
 	else if test -d $OMF_CONFIG/pkg/fastupdate
-		set -xl GIT_WORK_TREE $OMF_CONFIG/pkg/fastupdate
+		set GIT_WORK_TREE $OMF_CONFIG/pkg/fastupdate
 	end
 	git -C $GIT_WORK_TREE describe --always --long --dirty
 end
